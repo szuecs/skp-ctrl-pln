35e30c1454f6d1c8a636a904c1eeeb0a60735557
Sandor Szücs <sandor.szuecs@zalando.de>
2022-04-27 13:09:13 +0200

    remove old test
    Signed-off-by: Sandor Szücs <sandor.szuecs@zalando.de>

--- fabric/fabric_test.go
+++ fabric/fabric_test.go
@@ -6,9 +6,6 @@ import (
 	"github.com/szuecs/skp-ctrl-pln/fabric/fabrictest"
 )
 
-func TestFabricCluster(t *testing.T) {
-	fabrictest.FixturesToTest(t, "testdata/spp-cluster")
-}
 func TestFabricFeature(t *testing.T) {
 	fabrictest.FixturesToTest(t, "testdata/feature")
 }
